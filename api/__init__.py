b9b7cd0f32de29a7b8a27680b689284247756bfe
Patrick Allen <prallen90@gmail.com>
2016-01-03 17:32:59 -0500

    Use URIs instead of nested resources

--- api/__init__.py
+++ api/__init__.py
@@ -15,9 +15,16 @@ from api.polls.models import *
 
 # Load User Endpoints
 from api.users.resources import UsersList, UserItem
-api.add_resource(UsersList, '/users')
-api.add_resource(UserItem, '/users/<username>')
+api.add_resource(UsersList, '/users', endpoint='users')
+api.add_resource(UserItem, '/users/<username>', endpoint='user')
 
-from api.polls.resources import PollItem, PollsList
-api.add_resource(PollItem, '/polls/<poll_id>')
-api.add_resource(PollsList, '/polls')
+from api.polls.resources import PollAPI, PollListAPI, ChoiceAPI, ChoiceListAPI
+api.add_resource(PollListAPI, '/polls', endpoint='polls')
+api.add_resource(PollAPI, '/polls/<int:id>', endpoint='poll')
+
+api.add_resource(ChoiceAPI, '/choice/<int:id>', endpoint='choice')
+api.add_resource(
+    ChoiceListAPI,
+    '/polls/<int:id>/choices',
+    endpoint='poll_choices'
+)
--- api/polls/resources.py
+++ api/polls/resources.py
@@ -1,16 +1,17 @@
-from api import api, db
+from api import db
 from flask_restful import Resource, marshal_with, fields, reqparse
 from api.polls.models import Choice, Poll
 
 
 choice_fields = {
     'text': fields.String,
-    'id': fields.Integer
+    'votes': fields.Integer,
+    'uri': fields.Url('choice')
 }
 poll_fields = {
-    'id': fields.Integer,
     'text': fields.String,
-    'choices': fields.List(fields.Nested(choice_fields))
+    'choices': fields.Url('poll_choices'),
+    'uri': fields.Url('poll')
 }
 
 poll_parser = reqparse.RequestParser()
@@ -22,24 +23,24 @@ poll_parser.add_argument(
 )
 
 
-class PollItem(Resource):
+class PollAPI(Resource):
 
     @marshal_with(poll_fields)
-    def get(self, poll_id):
-        poll = Poll.query.get(poll_id)
+    def get(self, id):
+        poll = Poll.query.get(id)
         return poll
 
     @marshal_with(poll_fields)
-    def put(self, poll_id):
+    def put(self, id):
         args = poll_parser.parse_args()
-        poll = Poll.query.get(poll_id)
+        poll = Poll.query.get(id)
         poll.text = args.text
         db.session.add(poll)
         db.session.commit()
         return poll
 
 
-class PollsList(Resource):
+class PollListAPI(Resource):
 
     @marshal_with(poll_fields)
     def get(self):
@@ -52,3 +53,16 @@ class PollsList(Resource):
         db.session.add(poll)
         db.session.commit()
         return poll
+
+
+class ChoiceAPI(Resource):
+
+    @marshal_with(choice_fields)
+    def get(self, id):
+        return Choice.query.get(id)
+
+
+class ChoiceListAPI(Resource):
+    @marshal_with(choice_fields)
+    def get(self, id):
+        return Poll.query.get(id).choices
