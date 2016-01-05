97041f5ae24a34703b4e31f126df4f9ef150fbbc
Patrick Allen <prallen90@gmail.com>
2016-01-05 07:43:58 -0500

    Set cascade on poll choices so that they are deleted when the poll is

--- api/polls/models.py
+++ api/polls/models.py
@@ -6,7 +6,7 @@ class Poll(BaseMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(140), nullable=False)
     owner = db.Column(db.Integer, db.ForeignKey('user.id'))
-    choices = db.relationship('Choice', backref='poll')
+    choices = db.relationship('Choice', backref='poll', cascade='all')
 
     def __init__(self, text):
         self.text = text
