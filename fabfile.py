942d2a85a8d4773b7a183a9db4a54feb17242180
Patrick Allen <prallen90@gmail.com>
2016-01-14 23:11:29 -0500

    Add fab command to install apt packages

--- fabfile.py
+++ fabfile.py
@@ -10,7 +10,14 @@ connection = "%s@%s" % (env.user, env.host)
 virtualenv = "/var/%senv" % app_name
 root = "/var/%s" % app_name
 
-
+apt_packages = [
+	'python-pip',
+	'postgresql',
+	'nginx',
+	'htop',
+	'vim',
+	'redis-server',
+]
 @hosts(connection)
 def runserver(host="0.0.0.0"):
     with cd(root):
@@ -28,9 +35,15 @@ def db(task):
     with cd(root):
         run("%s/bin/python manage.py db %s" % (virtualenv, task))
 
+
 @hosts(connection)
 def setup_db():
 	sudo("apt-get install postgresql-server-dev-all postgresql postgresql-contrib -y")
 	run("sudo -u postgres createuser --superuser vagrant")
 	run("sudo -u postgres psql -c \"ALTER USER vagrant WITH PASSWORD 'vagrant';\"")
 	run("sudo -u vagrant createdb -O vagrant apidb")
+
+
+@hosts(connection)
+def apt_install():
+	sudo("apt-get install %s -y" % " ".join(apt_packages))
